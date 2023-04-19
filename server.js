6d79cb5f10724a8b3311777fd337e464165d7534
Ani-Kandikattu <anikandikattu@gmail.com>
2023-04-19 11:59:03 -0700

    created main menu function and added inquirer prompts for main menu options and also added switch case statements for users cycling through the options

--- server.js
+++ server.js
@@ -1,3 +1,60 @@
-const mysql = requrie('mysql2');
-const inquirer = require('inquirer');
-const cTable = require('console.table');
+const mysql = requrie("mysql2");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "company_db",
+  },
+  console.log("Connected to the books_db databse.")
+);
+
+const mainMenu = () => {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "choice",
+      message: "Main Menu",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+        "Exit",
+      ],
+    })
+    .then((answers) => {
+      switch (answers.choice) {
+        case "View all departments":
+          openAllDepartments();
+          break;
+        case "View all roles":
+          openAllRoles();
+          break;
+        case "View all employees":
+          openAllEmployees();
+          break;
+        case "Add a department":
+          addDepartment();
+          break;
+        case "Add a role":
+          addRole();
+          break;
+        case "Add an employee":
+          addEmployee();
+          break;
+        case "Update an employee role":
+          updateEmployee();
+          break;
+        case "Exit":
+          return;
+      }
+      mainMenu();
+    });
+};
